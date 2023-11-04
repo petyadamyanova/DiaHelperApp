29c43fbe69cdd1a62a8157251512d0afabc0fed9
Petya Damyanova <>
2023-11-04 10:38:04 +0200

    adding homeVc, graphicsVc and profileVc to the tabBar

---
+++ DiaHelperApplication/DiaHelperApplication/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  DiaHelperApplication
+//
+//  Created by Petya Damyanova on 4.11.23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+    }
+
+}
---
+++ DiaHelperApplication/DiaHelperApplication/Controllers/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  DiaHelperApplication
+//
+//  Created by Petya Damyanova on 4.11.23.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray4
+
+    }
+
+}
