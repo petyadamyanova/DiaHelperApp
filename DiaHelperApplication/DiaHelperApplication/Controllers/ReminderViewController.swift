0a21aa362e4bdb6d1e85963003cbaa57ca99de66
Petya Damyanova <>
2023-11-26 16:06:19 +0200

    adding comment

--- DiaHelperApplication/DiaHelperApplication/Controllers/ReminderViewController.swift
+++ DiaHelperApplication/DiaHelperApplication/Controllers/ReminderViewController.swift
@@ -14,6 +14,7 @@ class ReminderViewController: UIViewController {
         view.backgroundColor = .systemGray6
         setupDismissButton()
     }
+    // h
     
     private func setupDismissButton() {
         let cancelAction = UIAction(handler: didTapCancelButton)
