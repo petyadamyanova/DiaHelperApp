2bfddaf49341cb008a7dbf5b79a5c600ffd3e68c
Petya Damyanova <>
2023-12-04 20:15:38 +0200

    adding bottom constraint

--- DiaHelperApplication/DiaHelperApplication/Controllers/StartViewController.swift
+++ DiaHelperApplication/DiaHelperApplication/Controllers/StartViewController.swift
@@ -111,7 +111,9 @@ class StartViewController: UIViewController {
         view.addConstraints([
             stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
             stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50)
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -300)
+                
         ])
     }
 }
