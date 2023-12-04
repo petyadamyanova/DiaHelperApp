3fc4fb99d8196366cb2b1b6d92625831ad7d146d
Petya Damyanova <>
2023-12-04 20:22:44 +0200

    moving color extension to a separate file in Extensions folder

---
+++ DiaHelperApplication/DiaHelperApplication/Extensions/UIColor+Hex.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Hex.swift
+//  DiaHelperApplication
+//
+//  Created by Petya Damyanova on 4.12.23.
+//
+
+import Foundation
+
+import UIKit
+
+extension UIColor {
+    convenience init(hex: UInt32, alpha: CGFloat = 1.0) {
+        let red = CGFloat((hex & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((hex & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(hex & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
